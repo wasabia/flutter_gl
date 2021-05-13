c6884ae69544a9112b54fcbca97a8dd924d443c6
zl <themorecolor@gmail.com>
2021-05-13 21:20:59 +0800

    remove macos

--- flutter_gl/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_gl/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_gl
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterGlPlugin.register(with: registry.registrar(forPlugin: "FlutterGlPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
