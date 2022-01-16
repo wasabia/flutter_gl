75d9ccd033369d0089887433fbe36ba72c0ebc9f
zl <themorecolor@gmail.com>
2022-01-16 20:02:20 +0800

    commit

--- flutter_gl/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_gl/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_gl_macos
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
